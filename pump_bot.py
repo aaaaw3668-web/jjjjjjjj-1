ac03ddc85cc02e78bef4156274f2794f3048b7e7
aaaaw3668-web <aaaaw3668@gmail.com>
2026-03-16 23:00:03 +0500

    Update pump_bot.py

--- pump_bot.py
+++ pump_bot.py
@@ -7,7 +7,7 @@ import atexit
 
 # Настройки
 TELEGRAM_BOT_TOKEN = '7446722367:AAFfl-bNGvYiU6_GpNsFeRmo2ZNZMJRx47I'
-PRICE_INCREASE_THRESHOLD = 1.5  # Порог для роста цены
+PRICE_INCREASE_THRESHOLD = 5  # Порог для роста цены
 PRICE_DECREASE_THRESHOLD = -50  # Порог для падения цены
 TIME_WINDOW = 60 * 5
 MAX_ALERTS_PER_DAY = 3
